1a8a7a4f70fe2a23c98a533e4761f9689be82d4b
Rajiv Seelam <rajiv@betalectic.com>
2024-11-02 20:03:49 +0530

    change og url

--- plugins/docusaurus-og-bolt-cdn-plugin/index.js
+++ plugins/docusaurus-og-bolt-cdn-plugin/index.js
@@ -68,7 +68,7 @@ module.exports = function(context, options) {
           
           // Construct the full URL for this page
           const pageUrl = `${siteConfig.url}${route.path}`;
-          const ogboltUrl = `https://og-bolt-image-generator.betalectic.workers.dev?url=${encodeURIComponent(pageUrl)}`;
+          const ogboltUrl = `https://ogbolt.locoworks.org?url=${encodeURIComponent(pageUrl)}`;
           
           // Remove existing og:image tags
           $('meta[property="og:image"]').remove();
