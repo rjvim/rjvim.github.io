a263378df1f4058ae4700558303a0ae1a8175039
Rajiv Seelam <rajiv@betalectic.com>
2025-01-15 11:19:56 +0530

    Update Docusaurus and Tailwind CSS dependencies; enhance Tailwind configuration

--- plugins/docusaurus-tailwindcss-loader/index.js
+++ plugins/docusaurus-tailwindcss-loader/index.js
@@ -1,35 +1,10 @@
 module.exports = function (context, options) {
   return {
-    name: "postcss-tailwindcss-loader",
-    // https://github.com/facebook/docusaurus/issues/2961#issuecomment-1531243979
-    injectHtmlTags({content}) {
-      return {
-        headTags: [
-          {
-            tagName: "link",
-            attributes: {
-              rel: "stylesheet",
-              href: "https://cdn.jsdelivr.net/npm/tailwindcss/dist/preflight.min.css",
-            },
-          },
-        ],
-      };
-    },
+    name: 'postcss-tailwindcss-loader',
+    // https://github.com/facebook/docusaurus/issues/2961
     configurePostCss(postcssOptions) {
-      // Appends new PostCSS plugin.
-      //   postcssOptions.plugins.push(
-      //     require("postcss-import"),
-      //     require("tailwindcss"),
-      //     require("postcss-preset-env")({
-      //       autoprefixer: {
-      //         flexbox: "no-2009",
-      //       },
-      //       stage: 4,
-      //     })
-      //   );
-      // https://github.com/facebook/docusaurus/issues/2961#issuecomment-1531243979
-      postcssOptions.plugins.push(require("tailwindcss"));
-      postcssOptions.plugins.push(require("autoprefixer"));
+      postcssOptions.plugins.push(require('tailwindcss'));
+      postcssOptions.plugins.push(require('autoprefixer'));
       return postcssOptions;
     },
   };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,67 @@
-import typographyPlugin from "@tailwindcss/typography";
 import { type Config } from "tailwindcss";
-import typographyStyles from "./typography";
+import {
+  scopedPreflightStyles,
+  isolateInsideOfContainer,
+} from "tailwindcss-scoped-preflight";
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.tsx"],
-  corePlugins: { preflight: false },
+  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.tsx", "./*.ts"],
   darkMode: ["class", '[data-theme="dark"]'],
+  important: true,
+
   theme: {
-    fontSize: {
-      xs: ["0.8125rem", { lineHeight: "1.5rem" }],
-      sm: ["0.875rem", { lineHeight: "1.5rem" }],
-      base: ["1rem", { lineHeight: "1.75rem" }],
-      lg: ["1.125rem", { lineHeight: "1.75rem" }],
-      xl: ["1.25rem", { lineHeight: "2rem" }],
-      "2xl": ["1.5rem", { lineHeight: "2rem" }],
-      "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
-      "4xl": ["2rem", { lineHeight: "2.5rem" }],
-      "5xl": ["3rem", { lineHeight: "3.5rem" }],
-      "6xl": ["3.75rem", { lineHeight: "1" }],
-      "7xl": ["4.5rem", { lineHeight: "1" }],
-      "8xl": ["6rem", { lineHeight: "1" }],
-      "9xl": ["8rem", { lineHeight: "1" }],
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
-    typography: typographyStyles,
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [typographyPlugin],
+
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    scopedPreflightStyles({
+      isolationStrategy: isolateInsideOfContainer(".twp", {
+        except: ".no-twp",
+      }),
+    }),
+  ],
 } satisfies Config;
