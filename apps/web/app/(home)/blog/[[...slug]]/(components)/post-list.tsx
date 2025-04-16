28896da34583b8f1a7ae738abe4217c2388764c4
Rajiv Seelam <rajiv@betalectic.com>
2025-04-16 11:00:58 +0530

    Remove text-left class from blog post list headings and descriptions

--- apps/web/app/(home)/blog/[[...slug]]/(components)/post-list.tsx
+++ apps/web/app/(home)/blog/[[...slug]]/(components)/post-list.tsx
@@ -33,10 +33,10 @@ export function PostList({
         <GridBackground maxWidthClass="container" />
 
         <div className="text-center">
-          <DocsTitle className="text-left dark:text-white capitalize">
+          <DocsTitle className="dark:text-white capitalize">
             {heading}
           </DocsTitle>
-          <DocsDescription className="text-left mt-3 dark:text-gray-300 mb-0">
+          <DocsDescription className="mt-3 dark:text-gray-300 mb-0">
             {description}
           </DocsDescription>
         </div>
