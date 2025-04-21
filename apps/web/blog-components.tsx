169f87ef8a366ae9899db70d046821d21ead2be8
Rajiv Seelam <rajiv@betalectic.com>
2025-04-21 18:01:32 +0530

    Remove unused imports and components from blog-components.tsx

--- apps/web/blog-components.tsx
+++ apps/web/blog-components.tsx
@@ -1,16 +1,7 @@
-import { PostCard } from "./app/(home)/blog/[[...slug]]/(components)/post-card";
 import { PostCard2 } from "./app/(home)/blog/[[...slug]]/(components)/post-card-2";
 import type { Metadata } from "next/types";
 import { GridBackground } from "@repo/ui/components/grid-background";
-import type { Component, ReactElement, ReactNode } from "react";
-import type { MDXComponents } from "mdx/types";
 import type { BlogComponents } from "./app/(home)/blog/[[...slug]]/(components)/types";
-import {
-  DocsPage,
-  DocsBody,
-  DocsDescription,
-  DocsTitle,
-} from "fumadocs-ui/page";
 import { Button } from "@repo/shadverse/components/button";
 import {
   Popover,
